776bf5a4136163330dfc1e89ca779281f358ccef
Jan Singon Slaný <slanyj@centrum.cz>
2016-07-12 22:06:31 +0200

    Add chaniso (isolate custom channel)

---
+++ src/chaniso.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+#
+# Creates a new image with duplicates of what is visible in the current image,
+# each scaled to a standard icon size.
+#
+
+from gimpfu import *
+
+def plugin_main(img, drawable):  
+    active = pdb.gimp_image_get_active_channel(img)
+    channels = img.channels
+    for channel in channels:
+        if channel==active:
+            channel.visible = True
+        else:
+            channel.visible = False
+
+# Placed in Channel context menu
+register(
+        "python_chaniso",
+        "Isolate this channel",
+        "Hide all custom channels except the active one.",
+        "Jan Singon Slany",
+        "Jan Singon Slany",
+        "2016",
+        "Isolate Channel",
+        "RGB*, GRAY*",
+        [
+            (PF_IMAGE, "image", "Input image", None),
+            (PF_DRAWABLE, "drawable", "Input drawable", None)
+        ],
+        [],
+        plugin_main,
+        "<Channels>")
+
+# Placed in main Layer menu
+"""register(
+        "python_chaniso",
+        "Isolate this channel",
+        "Hide all layers except the active one.",
+        "Jan Singon Slany",
+        "Jan Singon Slany",
+        "2016",
+        "Isolate Channel",
+        "RGB*, GRAY*",
+        [
+            (PF_IMAGE, "image", "Input image", None),
+            (PF_DRAWABLE, "drawable", "Input drawable", None)
+        ],
+        [],
+        plugin_main,
+        "<Image>/Layer")"""
+
+main()
